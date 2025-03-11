c891e69fc1d342d6852f5a040ccb080cafde2d64
aneesameen <aneesameen00@gmail.com>
2025-03-11 20:43:43 +0530

    third commit

--- Backend/constants.js
+++ Backend/constants.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5173';
+// const BASE_URL = 'http://localhost:5173';
+const BASE_URL = 'https://deploydemo-fe.vercel.app';
 // const BASE_URL = 'https://ai-traveller.vercel.app';
 
 module.exports = BASE_URL;
