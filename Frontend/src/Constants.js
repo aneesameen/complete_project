6962db7d12bf143a5ecf5797e631b151d192be94
aneesameen <aneesameen00@gmail.com>
2025-03-11 20:40:21 +0530

    second commit

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/';
+// export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL = 'https://deploydemo-bc.vercel.app/';
 // export const BASE_URL = 'https://ai-traveller-backend.onrender.com/';
